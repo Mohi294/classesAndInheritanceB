e53af31475e7bcc61ef2151e24c204a1469a1470
sosamo <sosamo@sosamo.s>
2021-03-11 11:39:48 +0330

    inheritance problem and super usage and __init__ misspelling also learned to make class objects

--- classesAndInstances.py
+++ classesAndInstances.py
@@ -1,15 +1,57 @@
 class bodyParts:
-    def _init_(self):
+    def __init__(self):
         self.hand = "hand"
         self.foot = "foot"
 
+    
+
 class creatures:
     def _init_(self, name):
+        self.bP = self.bodyPart()
         self.name = name
 
     class bodyPart(bodyParts):
         def display(self):
-            return "hi"
+            super().__init__() 
+        
+        def display(self):
+            return self.hand
+
+x = creatures().bodyPart().display()
+# y = x.bodyPart()
+print(x)
 
 
-print(creatures.bodyPart().ToString())
+# class A:
+#     def __init__(self):
+#         self.db = self.Inner()
+
+#     def display(self):
+#         print('In Parent Class')
+
+#     # this is inner class
+#     class Inner:
+
+#         def display1(self):
+#             print('Inner Of Parent Class')
+
+
+# class B(A):
+#     def __init__(self):
+#         print('In Child Class')
+#         super().__init__()
+
+#     class Inner(A.Inner):
+
+#         def display2(self):
+#             print('Inner Of Child Class')
+
+
+# # creating child class object
+# p = B()
+# p.display()
+
+# # create inner class object
+# x = p.db
+# x.display1()
+# x.display2()
