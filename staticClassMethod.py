edf60ec197e5ca28ddb3164110a0d2f343354d56
sosamo <sosamo@sosamo.s>
2021-03-13 02:10:42 +0330

    try static classes

---
+++ staticClassMethod.py
@@ -0,0 +1,27 @@
+# class Calculator:
+
+#     # create addNumbers static method
+#     # @staticmethod
+#     def addNumbers(self):
+#         return 55
+
+# c = Calculator()
+# print('Product:', c.addNumbers())
+
+class MyClass:
+    def method(self):
+        return 'instance method called', self
+
+    @classmethod
+    def classmethod(cls):
+        return 'class method called', cls
+
+    @staticmethod
+    def staticmethod():
+        return 'static method called'
+
+new_line = "\n"
+obj = MyClass()
+print(obj.method(){new_line}
+obj.classmethod(){new_line}
+obj.staticmethod())
